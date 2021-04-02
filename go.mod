feb139a50466815af7c952cba45fd2bda168f12a
Raphael Mazelier <rmazelier@meilleursagents.com>
2021-04-02 17:39:30 +0200

    first_commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github/ut0mt8/gcp_test
+
+go 1.16
+
+require (
+	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c
+	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558
+	google.golang.org/api v0.43.0
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/api/compute/v1"
+	"os"
+)
+
+func main() {
+
+	project := flag.String("project", "", "GCP project ID (Required)")
+	flag.Parse()
+
+	if *project == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+
+	}
+
+	ctx := context.Background()
+	computeService, err := compute.NewService(ctx)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	aggrList, err := computeService.Instances.AggregatedList(*project).Do()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for zone, values := range aggrList.Items {
+		if values.Instances != nil {
+			for _, instance := range values.Instances {
+				ip := "none"
+				lifecycle := "undef"
+				for _, nif := range instance.NetworkInterfaces {
+					ip = nif.NetworkIP
+				}
+				for _, meta := range instance.Metadata.Items {
+					if meta.Key == "lifecycle" {
+						lifecycle = *meta.Value
+					}
+				}
+				fmt.Printf("%s\t%s\t%s\t%s\t%s\n", zone, instance.Name, ip, instance.Status, lifecycle)
+			}
+		}
+	}
+}
